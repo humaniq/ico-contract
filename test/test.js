50c194e28af431308a1a1d4f54cc3392bf088d96
Alex Bash <avbashlykov@gmail.com>
2017-02-01 17:02:54 +0300

    tests fixed

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var gasAmount = 4000000;
 var baseTokenPrice = web3.toWei(0.001, "Ether");
 
 // Multisig address
-var multisig = "0xaec3ae5d2be00bfc91597d7a1b2c43818d84396a"
+var multisig = "0xaec3ae5d2be00bfc91597d7a1b2c43818d84396a";
 
 contract('HumaniqICO', function(accounts) {
     // Owner of the contract
@@ -10,7 +10,7 @@ contract('HumaniqICO', function(accounts) {
     // Regular HumaniQ investor
     var icoInvestor = accounts[1];
 
-    it("Should start ICO", function() {
+    it("Should start ICO", function(done) {
         // ICO Contract
         var icoContract = HumaniqICO.deployed();
 
@@ -30,10 +30,10 @@ contract('HumaniqICO', function(accounts) {
         }).then(function(isICOActive) {
             // check that ICO was successfuly activated
             assert.equal(isICOActive, true, "ICO is not active");
-        });
+        }).then(done);
     });
 
-    it("Should invest 10 ETH", function() {
+    it("Should invest 10 ETH", function(done) {
         // ICO Contract
         var icoContract = HumaniqICO.deployed();
         // Token contract
@@ -93,10 +93,10 @@ contract('HumaniqICO', function(accounts) {
         }).then(function(icoBalance) {
             // check that ICO balance is correct
             assert.equal(icoBalance.toNumber(), web3.toWei(10, "Ether"), "Wrong ICO balance");
-        });
+        }).then(done)
     });
 
-    it("Should invest 5 ETH via fundBTC()", function() {
+    it("Should invest 5 ETH via fundBTC()", function(done) {
         // ICO Contract
         var icoContract = HumaniqICO.deployed();
         // Token contract
@@ -150,10 +150,10 @@ contract('HumaniqICO', function(accounts) {
             assert.equal(icoBalance.toNumber(),
                 initialICOBalance + parseInt(web3.toWei(5, "Ether")),
                 "Wrong ICO balance");
-        });
+        }).then(done);
     });
 
-    it("Should return current bonus", function() {
+    it("Should return current bonus", function(done) {
         var icoContract = HumaniqICO.deployed();
 
         return icoContract.getBonus.call({
@@ -161,10 +161,10 @@ contract('HumaniqICO', function(accounts) {
         }).then(function(discount) {
             // check that getBonus() returns correct value
             assert.equal((discount - 1000) / 10, 49.9, "Wrong bonus");
-        });
+        }).then(done);
     });
 
-    it("Should finish crowdsale and allocate founder bonus", function() {
+    it("Should finish crowdsale and allocate founder bonus", function(done) {
         // ICO Contract
         var icoContract = HumaniqICO.deployed();
         // Token contract
@@ -173,24 +173,35 @@ contract('HumaniqICO', function(accounts) {
         var icoTotalBalance = 0;
         var totalCoinsIssued = 0;
 
-        return icoContract.finishCrowdsale({
-            from: icoOwner,
-            gas: gasAmount,
-            value: 0
-        }).then(function(tx_id) {
+        return icoContract.coinsIssued.call().then(function(coinsIssued) {
+            // save total number of tokens
+            totalCoinsIssued = coinsIssued.toNumber();
+
             return icoContract.icoBalance.call();
         }).then(function(icoBalance) {
+            // save ICO total balance
             icoTotalBalance = icoBalance.toNumber();
-            assert.equal(5, icoTotalBalance);
-            return icoContract.coinsIssued.call();
-        }).then(function(coinsIssued) {
 
-            var totalCoinsIssued = coinsIssued.toNumber();
-            var founderBonus = (coinsIssued / 86) * 14;
-            return tokenContract.balanceOf.call(multisig).then(function(balance) {
-                assert.equal(founderBonus, founderTokens,
-                    "Founders were not allocated with proper number of coins");
+            // finish crowdsale
+            return icoContract.finishCrowdsale({
+                from: icoOwner,
+                gas: gasAmount,
+                value: 0
             });
-        });
+        }).then(function(tx_id) {
+            return icoContract.isICOActive.call();
+        }).then(function(isICOActive) {
+            // check that ICO was closed
+            assert.equal(isICOActive, false, "ICO wasn't closed");
+            
+            return tokenContract.balanceOf.call(multisig);
+        }).then(function(founderTokens) {
+            // founders are supposed to receive 14% of all issued tokens
+            var founderBonus = Math.floor((totalCoinsIssued / 86) * 14);
+
+            // check that founders received proper number of tokens
+            assert.equal(founderBonus, founderTokens.toNumber(), "Founders were not allocated with proper number of coins");
+        }).then(done);
     });
+
 });
